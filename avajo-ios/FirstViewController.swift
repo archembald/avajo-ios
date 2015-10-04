b05c1a6d040725ec5b40b36be1f26158cbfdc452
Alejandro U. Alvarez <aluralma@gmail.com>
2015-10-03 20:44:03 -0400

    Initial Commit

---
+++ avajo-ios/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  avajo-ios
+//
+//  Created by Alejandro Alvarez Martinez on 03/10/2015.
+//  Copyright (c) 2015 Archembald. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
