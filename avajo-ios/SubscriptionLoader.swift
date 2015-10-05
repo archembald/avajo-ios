608bd83e7e920891f1f5bb5d265e1b7e4a8b36df
Alejandro U. Alvarez <aluralma@gmail.com>
2015-10-05 14:32:38 -0400

    add Meteor UI helpers

--- avajo-ios/SubscriptionLoader.swift
+++ avajo-ios/SubscriptionLoader.swift
@@ -1,7 +1,3 @@
-//
-//  SubscriptionsLoader.swift
-//  avajo-ios
-//
 // Copyright (c) 2014-2015 Martijn Walraven
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -25,53 +21,53 @@
 import Meteor
 
 protocol SubscriptionLoaderDelegate: class {
-    func subscriptionLoader(subscriptionLoader: SubscriptionLoader, subscription: METSubscription, didFailWithError error: NSError)
+  func subscriptionLoader(subscriptionLoader: SubscriptionLoader, subscription: METSubscription, didFailWithError error: NSError)
 }
 
 class SubscriptionLoader {
-    weak var delegate: SubscriptionLoaderDelegate? = nil
-    private var subscriptions: [METSubscription] = []
-    
-    deinit {
-        removeAllSubscriptions()
+  weak var delegate: SubscriptionLoaderDelegate? = nil
+  private var subscriptions: [METSubscription] = []
+  
+  deinit {
+    removeAllSubscriptions()
+  }
+  
+  func addSubscriptionWithName(name: String, parameters: AnyObject...) -> METSubscription {
+    let subscription = Meteor.addSubscriptionWithName(name, parameters: parameters)
+    subscription.whenDone { (error) -> Void in
+      if let error = error {
+        self.delegate?.subscriptionLoader(self, subscription: subscription, didFailWithError: error)
+      }
+    }
+    subscriptions.append(subscription)
+    return subscription
+  }
+  
+  func removeAllSubscriptions() {
+    for subscription in subscriptions {
+      Meteor.removeSubscription(subscription)
+    }
+  }
+  
+  var isReady: Bool {
+    return all(subscriptions, predicate: {$0.ready})
+  }
+  
+  func whenReady(handler: () -> Void) {
+    // Invoke completion handler synchronously if we're ready now
+    if isReady {
+      handler()
+      return
     }
     
-    func addSubscriptionWithName(name: String, parameters: AnyObject...) -> METSubscription {
-        let subscription = Meteor.addSubscriptionWithName(name, parameters: parameters)
-        subscription.whenDone { (error) -> Void in
-            if let error = error {
-                self.delegate?.subscriptionLoader(self, subscription: subscription, didFailWithError: error)
-            }
-        }
-        subscriptions.append(subscription)
-        return subscription
+    let group = dispatch_group_create()
+    for subscription in subscriptions {
+      dispatch_group_enter(group)
+      subscription.whenDone { (error) -> Void in
+        dispatch_group_leave(group)
+      }
     }
     
-    func removeAllSubscriptions() {
-        for subscription in subscriptions {
-            Meteor.removeSubscription(subscription)
-        }
-    }
-    
-    var isReady: Bool {
-        return all(subscriptions, predicate: {$0.ready})
-    }
-    
-    func whenReady(handler: () -> Void) {
-        // Invoke completion handler synchronously if we're ready now
-        if isReady {
-            handler()
-            return
-        }
-        
-        let group = dispatch_group_create()
-        for subscription in subscriptions {
-            dispatch_group_enter(group)
-            subscription.whenDone { (error) -> Void in
-                dispatch_group_leave(group)
-            }
-        }
-        
-        dispatch_group_notify(group, dispatch_get_main_queue(), handler)
-    }
+    dispatch_group_notify(group, dispatch_get_main_queue(), handler)
+  }
 }
