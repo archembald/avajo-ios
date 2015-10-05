f8866b299eebce54d0c6f06ab7483e328975a038
Alejandro U. Alvarez <aluralma@gmail.com>
2015-10-05 10:51:34 -0400

    display fb profile info

--- avajo-ios/LoginView.swift
+++ avajo-ios/LoginView.swift
@@ -8,8 +8,10 @@ class LoginView: UIViewController, FBSDKLoginButtonDelegate {
         
         if (FBSDKAccessToken.currentAccessToken() != nil){
             // User is already logged in, do work such as go to next view controller.
+            println("FB: User is logged in")
             goToMainView()
         }else{
+            println("FB: User not logged in")
             let loginView : FBSDKLoginButton = FBSDKLoginButton()
             self.view.addSubview(loginView)
             loginView.center = self.view.center
@@ -22,6 +24,7 @@ class LoginView: UIViewController, FBSDKLoginButtonDelegate {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        println("FB: Memory warning")
     }
     
     // Facebook Delegate Methods
@@ -29,18 +32,26 @@ class LoginView: UIViewController, FBSDKLoginButtonDelegate {
     func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
         println("User Logged In")
         
-        if ((error) != nil)
-        {
-            // Process error
+        if ((error) != nil) {
+            // Process error (show alert for now)
+            let alertController = UIAlertController(title: "Error", message:
+                error.description, preferredStyle: UIAlertControllerStyle.Alert)
+            alertController.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default,handler: nil))
+            
+            self.presentViewController(alertController, animated: true, completion: nil)
         }
         else if result.isCancelled {
-            // Handle cancellations
+            // Handle cancellations (show alert for now)
+            let alertController = UIAlertController(title: "avajo", message:
+                "You'll need to login at some point!", preferredStyle: UIAlertControllerStyle.Alert)
+            alertController.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default,handler: nil))
+            
+            self.presentViewController(alertController, animated: true, completion: nil)
         }
         else {
             // If you ask for multiple permissions at once, you
             // should check if specific permissions missing
-            if result.grantedPermissions.contains("email")
-            {
+            if result.grantedPermissions.contains("email") {
                 // Do work
                 goToMainView()
             }
@@ -49,11 +60,18 @@ class LoginView: UIViewController, FBSDKLoginButtonDelegate {
     
     func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
         println("User Logged Out")
+        goToLoginView()
     }
     
     func goToMainView(){
         dispatch_async(dispatch_get_main_queue()) {
             self.performSegueWithIdentifier("loginSegue", sender: self)
+        }
+    }
+    
+    func goToLoginView(){
+        dispatch_async(dispatch_get_main_queue()) {
+            self.performSegueWithIdentifier("logoutSegue", sender: self)
         }
     }
     
---
+++ avajo-ios/ProfileViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ProfileViewController.swift
+//  avajo-ios
+//
+//  Created by Alejandro Alvarez Martinez on 05/10/2015.
+//  Copyright (c) 2015 Archembald. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    @IBOutlet var imgProfilePicture: UIImageView!
+    @IBOutlet var labelUsername: UILabel!
+    @IBOutlet var labelEmail: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.imgProfilePicture.layer.masksToBounds = true
+        self.imgProfilePicture.layer.cornerRadius = self.imgProfilePicture.frame.height/2
+        
+        // Get info from FB
+        let graphRequest : FBSDKGraphRequest = FBSDKGraphRequest(graphPath: "me", parameters: nil)
+        graphRequest.startWithCompletionHandler({ (connection, result, error) -> Void in
+            
+            if ((error) != nil){
+                // Process error
+                println("Error: \(error)")
+            }
+            else
+            {
+                self.labelUsername.text = result.valueForKey("name") as? String
+                self.labelEmail.text = result.valueForKey("email") as? String
+                
+                // Set pic
+                let fbid = result.valueForKey("id") as? String
+                let url = NSURL(string: "https://graph.facebook.com/"+fbid!+"/picture?type=large")
+                let data = NSData(contentsOfURL: url!)
+                self.imgProfilePicture.image = UIImage(data: data!);
+            }
+        })
+
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
